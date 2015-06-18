9c40dfd41f7c70e775f37d172858d05b71eb9371
rogrs <rogerio.rs@gmail.com>
2015-06-18 15:54:12 -0300

    #1 add scritp create tables

--- config/sql/ddl/create-all-tables.sql
+++ config/sql/ddl/create-all-tables.sql
@@ -26,14 +26,64 @@ CREATE TABLE IF NOT EXISTS `propriedades` (
 CREATE TABLE IF NOT EXISTS `logcommands` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `idusuario` bigint(20) NOT NULL,
-  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `command` varchar(80) NOT NULL,
   `info` varchar(1000),
-  `status` int(11) NOT NULL DEFAULT '1',
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enable` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY(`id`),
   KEY `fk_logcommand_usuario` (`idusuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
+
+CREATE TABLE IF NOT EXISTS `plugins` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `plugin` varchar(60) NOT NULL,
+  `command` varchar(100) NOT NULL,
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `scripts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idplugin` bigint(20) NOT NULL,
+  `script` varchar(60) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY(`id`),
+  KEY `fk_script_plugin` (`idplugin`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `projetos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `projeto` varchar(80) NOT NULL,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `planos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idprojeto` bigint(20) NOT NULL,
+  `plano` varchar(80) NOT NULL,
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY(`id`),
+  KEY `fk_plano_projeto` (`idprojeto`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `atividades` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idplano` bigint(20) NOT NULL,
+  `idscript` bigint(20) NOT NULL,
+  `idusuario` bigint(20) NOT NULL,
+  `atividade` varchar(80) NOT NULL,
+  `dtcreate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `enable` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY(`id`),
+  KEY `fk_atividade_plano` (`idplano`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
 
 
 
