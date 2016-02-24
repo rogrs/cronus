8f6678c63bd866294846aa924b0d289a58af441f
rogrs <rogerio.rs@gmail.com>
2016-02-24 00:21:57 -0300

    script start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 shome=`dirname $0`
-echo "-jar $shome/autobot-0.0.1-SNAPSHOT.war"
-java -jar "$shome/autobot-0.0.1-SNAPSHOT.war" &
+echo "-jar $shome/target/autobot-0.0.1-SNAPSHOT.war"
+java -jar "$shome/target/autobot-0.0.1-SNAPSHOT.war" &
