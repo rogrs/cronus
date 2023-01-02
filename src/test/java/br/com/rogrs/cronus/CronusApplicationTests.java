efc348fb1b0e871d5d8f0d88d7599c6c25d3043c
Rogerio Ribeiro <rogerio.ribeiro@mosaico.com.br>
2023-01-02 18:30:05 -0300

    Config plugin cobertura maven

--- src/test/java/br/com/rogrs/cronus/CronusApplicationTests.java
+++ src/test/java/br/com/rogrs/cronus/CronusApplicationTests.java
@@ -1,13 +1,21 @@
 package br.com.rogrs.cronus;
 
+import br.com.rogrs.cronus.web.rest.FilmsController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CronusApplicationTests {
 
+	@Autowired
+	private FilmsController filmsController;
+
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(filmsController);
 	}
 
 }
